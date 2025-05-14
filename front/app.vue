289a9289cca5e0bba1ee5054e75dc590b1a05aeb
yvan <yvan.langlois@etu.uca.fr>
2025-05-14 21:51:39 +0200

    mise en place de tailwind

--- front/app.vue
+++ front/app.vue
@@ -1,4 +1,6 @@
 <template>
-  <NuxtPage/>
-  <Background/>
+  <UApp>
+    <NuxtPage />
+    <Background/>
+  </UApp>
 </template>
--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-particles','@nuxt/fonts', '@nuxt/ui']
+  modules: ['nuxt-particles','@nuxt/fonts', '@nuxt/ui'],
+  css: ['~/assets/css/main.css']
 })
